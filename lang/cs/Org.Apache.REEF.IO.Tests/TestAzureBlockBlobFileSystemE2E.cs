a00599852ce45e35aa1d11ed6ac31cca2de60021
Dwaipayan <dwmukh@microsoft.com>
2018-01-23 11:55:59 -0800

    Adls File System implementation with End to End tests

--- lang/cs/Org.Apache.REEF.IO.Tests/TestAzureBlockBlobFileSystemE2E.cs
+++ lang/cs/Org.Apache.REEF.IO.Tests/TestAzureBlockBlobFileSystemE2E.cs
@@ -134,7 +134,7 @@ namespace Org.Apache.REEF.IO.Tests
         {
             var helloFilePath = PathToFile(HelloFile);
             var blob = _container.GetBlockBlobReference(HelloFile);
-            var tempFilePath = GetTempFilePath();
+            var tempFilePath = Path.GetTempFileName();
             const string text = "hello";
             try
             {
@@ -155,7 +155,7 @@ namespace Org.Apache.REEF.IO.Tests
             var helloFilePath = PathToFile(HelloFile);
             ICloudBlob blob = _container.GetBlockBlobReference(HelloFile);
             Assert.False(CheckBlobExists(blob));
-            var tempFilePath = GetTempFilePath();
+            var tempFilePath = Path.GetTempFileName();
             const string text = "hello";
             try
             {
@@ -248,11 +248,6 @@ namespace Org.Apache.REEF.IO.Tests
             }
 
             Assert.True(CheckContainerExists(_container));
-        }
-
-        private static string GetTempFilePath()
-        {
-            return Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
         }
 
         private static void UploadFromString(ICloudBlob blob, string str)
--- lang/cs/Org.Apache.REEF.IO.Tests/TestAzureDataLakeFileSystem.cs
+++ lang/cs/Org.Apache.REEF.IO.Tests/TestAzureDataLakeFileSystem.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Microsoft.Azure.DataLake.Store;
 using Microsoft.Azure.DataLake.Store.MockAdlsFileSystem;
+using NSubstitute;
 using Org.Apache.REEF.IO.FileSystem;
 using Org.Apache.REEF.IO.FileSystem.AzureDataLake;
-using Org.Apache.REEF.IO.FileSystem.AzureDatLake;
 using Org.Apache.REEF.Tang.Implementations.Tang;
 using Xunit;
 
@@ -16,7 +11,7 @@ namespace Org.Apache.REEF.IO.Tests
 {
     public sealed class TestAzureDataLakeFileSystem
     {
-        private readonly static Uri FakeUri = new Uri("http://fake.com");
+        private readonly static Uri FakeUri = new Uri("http://fake.com/f1");
 
         [Fact]
         public void TestDoesNotExist()
@@ -29,19 +24,26 @@ namespace Org.Apache.REEF.IO.Tests
         public void TestExists()
         {
             var testContext = new TestContext();
-            testContext.mockAdlsClient.CreateFile(FakeUri.ToString(), IfExists.Overwrite);
+            testContext.mockAdlsClient.CreateFile(FakeUri.AbsolutePath, IfExists.Overwrite);
             Assert.True(testContext.GetAdlsFileSystem().Exists(FakeUri));
         }
 
         private sealed class TestContext
         {
             public readonly AdlsClient mockAdlsClient = MockAdlsClient.GetMockClient();
+            public readonly IDataLakeStoreClient TestDataLakeStoreClient = Substitute.For<IDataLakeStoreClient>();
 
             public IFileSystem GetAdlsFileSystem()
             {
-                var conf = AzureDataLakeFileSystemConfiguration.ConfigurationModule.Build();
+                var conf = AzureDataLakeFileSystemConfiguration.ConfigurationModule
+                     .Set(AzureDataLakeFileSystemConfiguration.DataLakeStorageAccountName, "adlsAccountName")
+                    .Set(AzureDataLakeFileSystemConfiguration.Tenant, "tenant")
+                    .Set(AzureDataLakeFileSystemConfiguration.ClientId, "clientId")
+                    .Set(AzureDataLakeFileSystemConfiguration.SecretKey, "secretKey")
+                    .Build();
                 var injector = TangFactory.GetTang().NewInjector(conf);
-                injector.BindVolatileInstance(mockAdlsClient);
+                injector.BindVolatileInstance(TestDataLakeStoreClient);
+                TestDataLakeStoreClient.GetReference().ReturnsForAnyArgs(mockAdlsClient);
                 var fs = injector.GetInstance<AzureDataLakeFileSystem>();
                 return fs;
             }
